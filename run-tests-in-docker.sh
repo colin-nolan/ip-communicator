408699b2414f1cedf761f85f3fb7fc610b2aaf8c
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-12-09 14:35:02 +0000

    Adds fix for CodeCov.

--- run-tests-in-docker.sh
+++ run-tests-in-docker.sh
@@ -4,13 +4,11 @@ set -euf -o pipefail
 
 scriptDirectory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-## Note: `mktemp` creates temps in directory that cannot be mounted by default on Mac
-#tempDirectory="/tmp/patchwork-docker-${RANDOM}"
-#trap "echo ${tempDirectory} && rm -rf ${tempDirectory}" INT TERM HUP EXIT
-#mkdir "${tempDirectory}"
-
 docker build -t ipcommunicator -f Dockerfile .
 docker build -t ipcommunicator-tests -f Dockerfile.test .
-docker run --rm -it -v "${scriptDirectory}":/ipcommunicator \
+docker run  --rm -it -v "${scriptDirectory}":/ip-communicator \
     --entrypoint=bash \
     ipcommunicator-tests run-tests.sh
+
+# Hack the paths in the coverage report (unfortunately they are absolute)
+docker run --rm -it -v $PWD/:/data alpine sed -i -e "s|\"/ip-communicator|\"${scriptDirectory}|g" /data/.coverage
